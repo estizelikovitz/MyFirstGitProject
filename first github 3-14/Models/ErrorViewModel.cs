65feb9df6171f77587b26a47b5b669495cde792a
Esti Zelikovitz <estizelikovitz@gmail.com>
2023-02-01 14:20:37 -0500

    Add project files.

---
+++ first github 3-14/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace first_github_3_14.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
